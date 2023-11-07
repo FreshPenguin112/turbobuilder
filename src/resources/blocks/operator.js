1c973a2b24217027f301c5993c64f410a36ca102
Fresh <93781766+FreshPenguin112@users.noreply.github.com>
2023-11-07 10:07:02 -0600

    more values waw

--- src/resources/blocks/operator.js
+++ src/resources/blocks/operator.js
@@ -5,7 +5,43 @@ const categoryPrefix = 'operator_';
 const categoryColor = '#59C059';
 
 function register() {
-    // <true/false/random>
+    // number
+    registerBlock(`${categoryPrefix}number`, {
+        message0: '%1',
+        args0: [
+            {
+                "type": "field_number",
+                "name": "NUMBER",
+                "value": 0
+            }
+        ],
+        output: "Number",
+        inputsInline: true,
+        colour: categoryColor
+    }, (block) => {
+        const NUMBER = block.getFieldValue('NUMBER');
+        const code = `Number(${NUMBER})`;
+        return [code, javascriptGenerator.ORDER_NONE];
+    })
+    // text
+    registerBlock(`${categoryPrefix}text`, {
+        message0: '%1',
+        args0: [
+            {
+                "type": "field_input",
+                "name": "TEXT",
+                "text": ""
+            }
+        ],
+        output: "String",
+        inputsInline: true,
+        colour: categoryColor
+    }, (block) => {
+        const TEXT = block.getFieldValue('TEXT');
+        const code = `String(${JSON.stringify(TEXT)})`;
+        return [code, javascriptGenerator.ORDER_NONE];
+    })
+    // boolean
     registerBlock(`${categoryPrefix}boolean`, {
         message0: '%1',
         args0: [
